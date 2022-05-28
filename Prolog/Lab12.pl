4e5d10cadf1d8705aeb3d8c8d1883d6a45943827
Дмитрий Писаренко <dimapisarenko28@gmail.com>
2022-05-28 04:10:33 +0300

    Lab 12 - Task 11 - Rec down

--- Prolog/Lab12.pl
+++ Prolog/Lab12.pl
@@ -9,3 +9,8 @@ prime(N,I):-N mod I =\= 0,I1 is I - 1, prime(N,I1),!.
 spdu(N,1,0):-!.
 spdu(N,I,S):-I1 is I - 1, 0 is N mod I, prime(I), spdu(N,I1,S1), S is S1 + I,!.
 spdu(N,I,S):-I1 is I - 1, spdu(N,I1,S).
+
+spdd(N,S):-spdd(N,N,S,0).
+spdd(N,1,S,S):-!.
+spdd(N,I,S,C):-I1 is I - 1, 0 is N mod I, prime(I), C1 is C + I, spdd(N,I1,S,C1),!.
+spdd(N,I,S,C):-I1 is I - 1, spdd(N,I1,S,C),!.
