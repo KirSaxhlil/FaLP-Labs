aaf3bebdd3978604c1bc039b6fd54cf132b504fa
Дмитрий Писаренко <dimapisarenko28@gmail.com>
2022-05-28 04:18:36 +0300

    Lab 12 - Task 12 - Rec up

--- Prolog/Lab12.pl
+++ Prolog/Lab12.pl
@@ -14,3 +14,12 @@ spdd(N,S):-spdd(N,N,S,0).
 spdd(N,1,S,S):-!.
 spdd(N,I,S,C):-I1 is I - 1, 0 is N mod I, prime(I), C1 is C + I, spdd(N,I1,S,C1),!.
 spdd(N,I,S,C):-I1 is I - 1, spdd(N,I1,S,C),!.
+
+%task 12
+digit_sum(N,S):-digit_sum(N,S,0).
+digit_sum(0,S,S):-!.
+digit_sum(N,S,C):-D is N mod 10, N1 is N div 10, C1 is C + D, digit_sum(N1,S,C1),!.
+
+mpdu(N,1,1):-!.
+mpdu(N,I,S):-I1 is I - 1, 0 is N mod I, digit_sum(N,SS1), digit_sum(I,SS2), SS2 < SS1, mpdu(N,I1,S1), S is S1 * I,!.
+mpdu(N,I,S):-I1 is I - 1, mpdu(N,I1,S).
