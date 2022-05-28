b979be398d78956d044484c541883bff0478a9a6
Дмитрий Писаренко <dimapisarenko28@gmail.com>
2022-05-28 04:06:46 +0300

    Lab 12 - Task 11- Rec up

--- Prolog/Lab12.pl
+++ Prolog/Lab12.pl
@@ -1,4 +1,11 @@
+nod(X,0,X):-!.
+nod(X,Y,N):-X1 is X mod Y, nod(Y,X1,N).
+
+prime(N):-I is N - 1, prime(N,I).
+prime(N,1):-!.
+prime(N,I):-N mod I =\= 0,I1 is I - 1, prime(N,I1),!.
+
 %task 11
-spdu(N,0,0):-!.
-spdu(N,I,S):-I1 is I - 1, 0 is N mod I, nod(N,I,X), 1 is X, spdu(N,I1,S1), S is S1 + I.
+spdu(N,1,0):-!.
+spdu(N,I,S):-I1 is I - 1, 0 is N mod I, prime(I), spdu(N,I1,S1), S is S1 + I,!.
 spdu(N,I,S):-I1 is I - 1, spdu(N,I1,S).
